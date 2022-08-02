f263128162e2c49f69e3b011ac45563f70de7b7a
SimoneDavi55 <109947200+SimoneDavi55@users.noreply.github.com>
2022-08-02 20:24:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ let config = {
   minZoom: 5,
   maxZoom: 18,
 };
+
+
 // magnification with which the map will start
 const zoom = 18;
 // co-ordinates
@@ -30,7 +32,29 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 
+const radius = 10;
 
+const markOne = L.latLng(44.42447050979073, 8.817488551139833);
+const markTwo = L.latLng(44.424483073543385, 8.817740678787233);
+const markThree = L.latLng(44.424620917453346, 8.816753625869753);
+const markFour = L.latLng(44.42436437436044, 8.818368315696718);
+
+const fakeDB = [markOne, markTwo, markThree, markFour];
+
+
+map.whenReady(PlaceReserved);
+
+function PlaceReserved() {
+  
+  for (let i = 0; i < fakeDB.length;i++)
+  {
+    circle = new L.circle(fakeDB[i], {
+      radius: radius,
+      color: 'green',
+    })
+      .addTo(map); 
+  }
+}
 
 
 // add marker on click
@@ -42,21 +66,18 @@ function addMarker(e) {
   markerPlace.textContent = `Nuovo Marker: ${e.latlng.lat}, ${e.latlng.lng}`;
 
 
-  $.get("https://www.openstreetmap.org/query?lat="+e.latlng.lat+"&lon="+e.latlng.lng, function(data){
+  /*$.get("https://www.openstreetmap.org/query?lat="+e.latlng.lat+"&lon="+e.latlng.lng, function(data){
     console.log(data);
- });
-
-  
-
+ });*/
 
   var popupText = `<p>Posizione = </p> <p>${e.latlng.lat}</p> <p>${e.latlng.lng}</p> <button type="button" class="remove">Cancella Posizione</button> <button type="button" class="save">Salva Posizione</button>`;
 
 
-  if(flag)
+  if(isPlaceable() && isBeach(e) && isInFreeSpace(e.latlng))
   {
     circle = new L.circle(e.latlng, {
     
-    radius: 10,
+    radius: radius,
   })
     .addTo(map);    
 
@@ -68,10 +89,42 @@ function addMarker(e) {
 
     flag=false;
   }
-  
+}
 
-  
+function isInFreeSpace(latlng)
+{
+  if (getNearestReservedSpaces(latlng) <= radius*2 )
+  {
+    alert("Sei troppo vicino ad un altro marker, piazzati almeno a "+radius*2+" m");
+    return false;
+  }
+  else return true;
+}
 
+function getNearestReservedSpaces(latlng)
+{
+  var startingPoint = latlng;
+  var distMin = startingPoint.distanceTo(fakeDB[0]);
+  for (var i = 1; i < fakeDB.length;i++)
+  {
+    if (distMin>startingPoint.distanceTo(fakeDB[i]))
+    {
+      distMin = startingPoint.distanceTo(fakeDB[i]);
+      nearestPos = fakeDB[i];
+    }
+  }
+  return distMin;
+}
+
+function isBeach(e)
+{
+  //TODO
+  return true;
+}
+
+function isPlaceable ()
+{
+  return flag;
 }
 
 const markerPlace = document.querySelector(".marker-position");
